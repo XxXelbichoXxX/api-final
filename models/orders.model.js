61af537e87699f18d86baa4c11ef49ef5819ea8a
el pifas <vimapachecoro@ittepic.edu.mx>
2024-01-09 02:25:31 -0600

    CASI FINAL

--- models/orders.model.js
+++ models/orders.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
   _id: { type: String},  
-  numero_mesa: { type: Number},
+  numero_mesa: { type: String},
   user_mesero: { type: String},
   orden: [
     {
