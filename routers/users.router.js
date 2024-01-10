67739e6938f198c2da6e48560274fec65f1a2987
el pifas <vimapachecoro@ittepic.edu.mx>
2024-01-10 02:52:39 -0600

    API CASI FINAL CASI 2

--- routers/users.router.js
+++ routers/users.router.js
@@ -9,6 +9,6 @@ router.get ("/:userName", usersController.getUserInfo);
 router.post ("/login/", usersController.loginUser);
 router.put ("/:userName", usersController.updateUser);
 router.delete ("/:userName", usersController.deleteUser);
-router.get("/check-duplicates", usersController.checkDuplicates);
+router.get("/:check-duplicates", usersController.checkDuplicates);
 
 module.exports = router;
